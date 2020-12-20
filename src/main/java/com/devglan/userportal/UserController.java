7ef0d6254dcc19d7438c352e859702f71e4a2530
unknown <admin@nsdldepnb344.nsdl.co.in>
2020-12-20 18:06:50 +0530

    added code

---
+++ src/main/java/com/devglan/userportal/UserController.java
@@ -0,0 +1,44 @@
+package com.devglan.userportal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 36000)
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public User findOne(@PathVariable("id") int id){
+        return userService.findById(id);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+
+    @DeleteMapping(path ={"/{id}"})
+    public User delete(@PathVariable("id") int id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping(value="/")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+	
+   @GetMapping(value="/hello")
+    public String sayHello() {
+        return "Hello there!";
+    }
+}
